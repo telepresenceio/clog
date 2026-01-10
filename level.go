83948c443e24463b169e0f700cc5ef11c0a3fdf5
Thomas Hallgren <thomas@tada.se>
2026-01-10 15:50:38 +0100

    Improve tree level handling and testing context.

--- level.go
+++ level.go
@@ -1,9 +1,11 @@
 package clog
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"strings"
+	"sync/atomic"
 )
 
 // LevelTrace is the most verbose log level.
@@ -39,8 +41,9 @@ func (l LevelWithTrace) MarshalText() ([]byte, error) {
 	return []byte(l.String()), nil
 }
 
-// UnmarshalText parses a log level string into a Level. It panics if the lowercased version of
-// the string is not one of "trace", "debug", "info", "warn", "warning", or "error".
+// UnmarshalText will unmarshal a log level string into a Level.
+// An error is returned if the string is not one of "TRACE", "DEBUG", "INFO", "WARN", or "ERROR"
+// using case-insensitive comparison.
 //goland:noinspection GoMixedReceiverTypes
 func (l *LevelWithTrace) UnmarshalText(value []byte) error {
 	var sl slog.Level
@@ -70,4 +73,36 @@ func MustParseLevel(s string) slog.Level {
 		panic(err)
 	}
 	return l
+}
+
+type treeLevelKey struct{}
+
+// WithTreeLevel assigns the log level root to a child context which is returned. Children
+// of this context will inherit the level.
+func WithTreeLevel(ctx context.Context, level slog.Level) context.Context {
+	lv := int64(level)
+	return context.WithValue(ctx, treeLevelKey{}, &lv)
+}
+
+// TreeEnabled returns true if the root [slog.Level] is enabled for the given context.
+// This function is suitable as an argument to a [handler.LevelEnabler] option.
+func TreeEnabled(ctx context.Context, level slog.Level) bool {
+	if lvp, ok := ctx.Value(treeLevelKey{}).(*int64); ok {
+		return int64(level) >= atomic.LoadInt64(lvp)
+	}
+	return false
+}
+
+// SetTreeLevel sets the root [slog.Level] for the context where the [WithTreeLevel] was set, which
+// might be the provided context itself or any parent of that context. Any children of context holding
+// the root level is affected by this change.
+func SetTreeLevel(ctx context.Context, level slog.Level) bool {
+	if lvp, ok := ctx.Value(treeLevelKey{}).(*int64); ok {
+		oldLevel := atomic.LoadInt64(lvp)
+		newLevel := int64(level)
+		if newLevel != oldLevel {
+			return atomic.CompareAndSwapInt64(lvp, oldLevel, newLevel)
+		}
+	}
+	return false
 }
---
+++ testutil/ctx.go
@@ -0,0 +1,38 @@
+package testutil
+
+import (
+	"context"
+	"log/slog"
+	"testing"
+
+	"github.com/telepresenceio/clog"
+	"github.com/telepresenceio/clog/handler"
+)
+
+type trapError struct {
+	slog.Handler
+	t *testing.T
+}
+
+func (t *trapError) Handle(ctx context.Context, record slog.Record) (err error) {
+	if record.Level >= slog.LevelError {
+		args := make([]any, 0, 1+record.NumAttrs())
+		args = append(args, record.Message)
+		record.Attrs(func(a slog.Attr) bool { args = append(args, a); return true })
+		t.t.Error(args...)
+	} else {
+		err = t.Handler.Handle(ctx, record)
+	}
+	return err
+}
+
+// NewContext returns a context with a logger that writes to t.
+// If failOnError is true, errors are propagated to t.Error instead of being logged.
+func NewContext(t *testing.T, failOnError bool) context.Context {
+	out := OutputWriter(t)
+	h := handler.NewText(handler.Output(out), handler.EnabledLevel(slog.LevelDebug))
+	if failOnError {
+		h = &trapError{Handler: h, t: t}
+	}
+	return clog.WithLogger(context.Background(), slog.New(h))
+}
